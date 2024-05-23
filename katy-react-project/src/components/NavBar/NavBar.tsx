80d6da80d4dc0a6314ed20d6202119c6cf1a6c01
Kaitlyn Fernelius <kkin001@gmail.com>
2024-05-22 22:12:16 -0500

    Some styling.. but I think I may change it up tomorrow

---
+++ katy-react-project/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,12 @@
+import './NavBar.css'
+
+export default function NavBar() {
+    return(
+        <>
+            <div className="nav-body">
+                <div className='options-board'>
+                    <button className="button-style">Button Text</button>
+                </div>
+            </div>
+        </>)
+}
