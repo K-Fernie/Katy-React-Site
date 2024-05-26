9ea3ad16dfa60c859dca62fd2168e42815d5fec6
Kaitlyn Fernelius <kkin001@gmail.com>
2024-05-25 22:36:08 -0500

    updated

--- katy-react-project/vite.config.ts
+++ katy-react-project/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: 'Katy-React-Site/katy-react-project'
+    base: '/katy-react-project/'
 })
