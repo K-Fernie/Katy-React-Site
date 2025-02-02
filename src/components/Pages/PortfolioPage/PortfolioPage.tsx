017458db1b6b28110a3bf1b97c0b3d62d4d433d8
Kaitlyn Fernelius <kkin001@gmail.com>
2025-02-01 21:20:56 -0600

    Added Initial Config For Portfolio

--- src/components/Pages/PortfolioPage/PortfolioPage.tsx
+++ src/components/Pages/PortfolioPage/PortfolioPage.tsx
@@ -1,8 +1,31 @@
+import './PortfolioPage.css'
+import {useEffect, useState} from "react";
+
 export default function PortfolioPage() {
+    const [displayText, setDisplayText] = useState("");
+    const text = "Page In Progress...";
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            if (index < text.length) {
+                console.log(text[index])
+                setDisplayText(text.slice(0, index + 1));
+                index++;
+            } else {
+                clearInterval(interval);
+            }
+        }, 150);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div>
-            {/*TODO Make Portfolio Page*/}
-            Portfolio page works
+        <div className="hacker-screen">
+            <div>
+                <span className="typed-text">{displayText}</span>
+                <span className="cursor">_</span>
+            </div>
         </div>
-    )
+    );
 }
